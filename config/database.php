c546be5ade76d6c6cc6542b8525047022ffea093
Kn0ws <ja.kn0ws.dev@gmail.com>
2025-04-24 10:11:43 +0900

    Eloquentを除外

--- config/database.php
+++ config/database.php
@@ -3,49 +3,38 @@
 return [
     'default' => getenv('DB_CONNECTION') ?: 'mysql',
 
-    // for Eloquent Connection.
-    'eloquent' => [
-
+    'connections' => [
         'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => getenv('DB_HOST') ?: '127.0.0.1',
-            'port'      => getenv('DB_PORT') ?: 3306,
-            'database'  => getenv('DB_DATABASE') ?: 'app',
-            'username'  => getenv('DB_USERNAME') ?: 'root',
-            'password'  => getenv('DB_PASSWORD') ?: '',
-            'charset'   => getenv('DB_CHARSET') ?: 'utf8mb4',
-            'collation' => 'utf8mb4_general_ci',
-            'prefix'    => '',
+            'driver'   => 'mysql',
+            'host'     => getenv('DB_HOST') ?: '127.0.0.1',
+            'port'     => getenv('DB_PORT') ?: '3306',
+            'database' => getenv('DB_DATABASE') ?: 'mydb',
+            'username' => getenv('DB_USERNAME') ?: 'root',
+            'password' => getenv('DB_PASSWORD') ?: '',
+            'charset'  => 'utf8mb4',
         ],
 
         'pgsql' => [
-            'driver'    => 'pgsql',
-            'host'      => getenv('DB_HOST') ?: '127.0.0.1',
-            'port'      => getenv('DB_PORT') ?: 5432,
-            'database'  => getenv('DB_DATABASE') ?: 'app',
-            'username'  => getenv('DB_USERNAME') ?: 'postgres',
-            'password'  => getenv('DB_PASSWORD') ?: '',
-            'charset'   => getenv('DB_CHARSET') ?: 'utf8',
-            'prefix'    => '',
+            'driver'   => 'pgsql',
+            'host'     => getenv('DB_HOST') ?: '127.0.0.1',
+            'port'     => getenv('DB_PORT') ?: '5432',
+            'database' => getenv('DB_DATABASE') ?: 'mydb',
+            'username' => getenv('DB_USERNAME') ?: 'postgres',
+            'password' => getenv('DB_PASSWORD') ?: '',
         ],
 
         'sqlite' => [
-            'driver'    => 'sqlite',
-            'database'  => getenv('DB_DATABASE') ?: ':memory:',
-            'prefix'    => '',
+            'driver'   => 'sqlite',
+            'database' => getenv('DB_DATABASE') ?: __DIR__ . '/../database.sqlite',
         ],
 
         'sqlsrv' => [
-            'driver'    => 'sqlsrv',
-            'host'      => getenv('DB_HOST') ?: 'localhost',
-            'port'      => getenv('DB_PORT') ?: 1433,
-            'database'  => getenv('DB_DATABASE') ?: 'app',
-            'username'  => getenv('DB_USERNAME') ?: 'sa',
-            'password'  => getenv('DB_PASSWORD') ?: '',
-            'prefix'    => '',
+            'driver'   => 'sqlsrv',
+            'host'     => getenv('DB_HOST') ?: 'localhost',
+            'port'     => getenv('DB_PORT') ?: '1433',
+            'database' => getenv('DB_DATABASE') ?: 'mydb',
+            'username' => getenv('DB_USERNAME') ?: 'sa',
+            'password' => getenv('DB_PASSWORD') ?: '',
         ],
-    ],
-
-    'connections' => [
     ],
 ];
